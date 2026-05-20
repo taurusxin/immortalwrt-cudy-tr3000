cda11493d57aeb690a681896f9f498ec6902d9b8
TaurusXin <i@taurusxin.com>
2026-05-20 23:17:56 +0800

    feat: aurora theme

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,7 @@
 #   cp -r "$GITHUB_WORKSPACE/package/luci-compat-keep" package/
 # fi
 
-# git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-# git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
+git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
 # git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
 # git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
