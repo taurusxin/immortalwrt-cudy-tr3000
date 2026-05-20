2e9fe79cde0a435e7ca4325c830a899fc9e9aa27
TaurusXin <i@taurusxin.com>
2026-05-21 02:29:21 +0800

    feat: back to 112m

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,4 +27,4 @@ sed -i 's/ImmortalWrt/TR3000/g' package/base-files/files/bin/config_generate
 #        -e '/^IMG_PREFIX:=/ s/\($(SUBTARGET)\)/\1-$(BUILD_DATE)/' include/image.mk
 
 # set ubi to 122M
-sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
+# sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
